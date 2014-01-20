62a07942d6bee9197132e0fa9580d9b4c87a7ee7
Snaaky <julien.duquesnoy8@gmail.com>
2014-01-20 20:06:27 +0100

    commit schema.rb

--- Syra/db/schema.rb
+++ Syra/db/schema.rb
@@ -11,7 +11,95 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140120175625) do
+ActiveRecord::Schema.define(version: 20140120180545) do
+
+  create_table "addresses", force: true do |t|
+    t.integer  "number"
+    t.string   "street"
+    t.string   "postalCode"
+    t.string   "town"
+    t.float    "x"
+    t.float    "y"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "evaluations", force: true do |t|
+    t.integer  "note"
+    t.text     "comment"
+    t.integer  "proposition_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "evaluations", ["proposition_id"], name: "index_evaluations_on_proposition_id"
+
+  create_table "hobbies", force: true do |t|
+    t.string   "label"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "levels", force: true do |t|
+    t.integer  "levelUser"
+    t.integer  "XPUser"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "levels", ["user_id"], name: "index_levels_on_user_id"
+
+  create_table "propositions", force: true do |t|
+    t.boolean  "isPaid"
+    t.boolean  "isAccepted"
+    t.boolean  "motifCancelled"
+    t.date     "proposition"
+    t.text     "comment"
+    t.integer  "user_id"
+    t.integer  "service_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "propositions", ["service_id"], name: "index_propositions_on_service_id"
+  add_index "propositions", ["user_id"], name: "index_propositions_on_user_id"
+
+  create_table "services", force: true do |t|
+    t.string   "title"
+    t.integer  "price"
+    t.text     "description"
+    t.text     "disponibility"
+    t.boolean  "isGiven"
+    t.boolean  "isFinished"
+    t.integer  "address_id"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "services", ["address_id"], name: "index_services_on_address_id"
+  add_index "services", ["category_id"], name: "index_services_on_category_id"
+  add_index "services", ["user_id"], name: "index_services_on_user_id"
+
+  create_table "successes", force: true do |t|
+    t.string   "label"
+    t.string   "urlImageValidate"
+    t.string   "urlImageUnvalidate"
+    t.boolean  "locked"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "successes", ["user_id"], name: "index_successes_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
