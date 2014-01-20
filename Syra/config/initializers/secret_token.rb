8bc5c871f7246609ea2438971d9f550058b92492
Maxime <max@max-K53SD.(none)>
2014-01-20 18:15:10 +0100

    version 0 donnees

---
+++ Syra/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Syra::Application.config.secret_key_base = '7ebc95a605b2993c8aed6a7c6474f5ac57ffceab3f061c2842e4d99dd97514fff338dc200c60e01028d730110ada7a2ce355ca0275ca5e24528b6ea37c31716e'
