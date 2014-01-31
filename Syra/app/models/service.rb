6a1078cf24d5524206d015621407dc6e6a123eb7
Snaaky <julien.duquesnoy8@gmail.com>
2014-01-31 18:07:07 +0100

    test

--- Syra/app/models/service.rb
+++ Syra/app/models/service.rb
@@ -1,5 +1,18 @@
 class Service < ActiveRecord::Base
+
+  validates :title, presence: true ,:presence => {:message => 'Vous devez indiquer un titre'}
+  validates :category_id, presence: true ,:presence => {:message => 'Vous devez choisir une catégorie'}
+  validates :description, presence: true ,:presence => {:message => 'Vous devez indiquer une description'}
+  validates :price, presence: true ,:presence => {:message => 'Vous devez indiquer un prix'}
+  validates :price, :numericality => {:only_integer => true}, :numericality => {:message => '$("input.numeric").numeric() à intégrer'}
+
   belongs_to :address
   belongs_to :category
   belongs_to :user
+  
+  before_save :default_values
+  def default_values
+    self.isGiven = 0 if self.isGiven.nil?
+    self.isFinished = 0 if self.isFinished.nil?
+  end
 end
