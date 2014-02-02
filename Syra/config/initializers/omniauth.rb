e746f58fff10e0bf1c3811e7eb3fd6ca1f1500f4
aynnas <terry.mainvis@gmail.com>
2014-02-02 13:31:54 +0100

    f

--- Syra/config/initializers/omniauth.rb
+++ Syra/config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
    # provider :facebook, [APP ID], [SECRET KEY], {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
-   provider :facebook, "199279863613884", "5b0ecbdb8398cb870fc42d777300e384"
+   provider :facebook, "199279863613884", "5b0ecbdb8398cb870fc42d777300e384" , :scope => 'email', :provider_ignores_state => true
   # If you want to also configure for additional login services, they would be configured here.
 end
