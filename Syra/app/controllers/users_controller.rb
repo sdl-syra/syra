e7eb750a371f4d4e96a74648e1a3161f71ad785f
Olin <Prudhommemaxime@gmail.com>
2014-02-10 11:22:12 +0100

    Lundi matin- gem image

--- Syra/app/controllers/users_controller.rb
+++ Syra/app/controllers/users_controller.rb
@@ -81,7 +81,7 @@ class UsersController < ApplicationController
     @user = current_user
     @user.follow!(params[:user])
     logger.info "test"
-
+    #logger.info @user.following?(params[:user])
     respond_to do |format|
         format.html { redirect_to '/users', notice: 'User was successfully updated.' }
         format.json { head :no_content }
