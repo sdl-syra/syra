0d020207065292e7143b575dc7dd21dba36f38c0
Snaaky <julien.duquesnoy8@gmail.com>
2014-03-01 21:42:06 +0100

    modification des services et de la gestion des images, modification de l'accueil mais j'ai des problèmes avec le javascript, il faudrait qu'on en discute

--- Syra/app/models/service.rb
+++ Syra/app/models/service.rb
@@ -1,4 +1,5 @@
 class Service < ActiveRecord::Base
+  include ActiveModel::Dirty
 
   validates :title, presence: true ,:presence => {:message => 'Vous devez indiquer un titre'}
   validates :category_id, presence: true ,:presence => {:message => 'Vous devez choisir une catégorie'}
@@ -13,8 +14,41 @@ class Service < ActiveRecord::Base
   mount_uploader :image, ServiceImageUploader
   
   before_save :default_values
-  def default_values
+  after_save :delete_default_image
+  
+  def default_values    
+    self.address = self.user.address if self.address.nil?
     self.isGiven = 0 if self.isGiven.nil?
     self.isFinished = 0 if self.isFinished.nil?
+    if self.image_changed? and self.image_was.to_s!="/uploads/service/image/placeholder/default.png"
+      delete_old_images
+    end
   end
+  
+  # supprime les images contenues dans le dossier
+  def delete_old_images  
+    imgDir = "public/uploads/service/image/" + self.id.to_s
+    accepted_formats = [".jpg", ".jpeg", ".png", ".gif"]
+    # pour chaque fichier
+    Dir.foreach(imgDir) do |file|
+       if accepted_formats.include? File.extname(file.to_s)
+          File.unlink("#{imgDir}/#{file}")
+       end
+    end
+  end
+  
+  # supprime l'image uploade(grand format) pour ne conserver que les versions "thumb" et "index"
+  def delete_default_image 
+    if self.image_url.to_s!="/uploads/service/image/placeholder/default.png"
+      imgDir = "public/uploads/service/image/" + self.id.to_s
+      accepted_formats = [".jpg", ".jpeg", ".png", ".gif"]
+      # pour chaque fichier
+      Dir.foreach(imgDir) do |file|
+         if !file.to_s.include?("thumb_") and !file.to_s.include?("index_") and accepted_formats.include? File.extname(file.to_s)
+              File.unlink("#{imgDir}/#{file}")
+         end
+      end
+    end
+  end
+  
 end
--- Syra/app/uploaders/service_image_uploader.rb
+++ Syra/app/uploaders/service_image_uploader.rb
@@ -23,6 +23,9 @@ class ServiceImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+    "/uploads/service/image/placeholder/default.png"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -36,24 +39,26 @@ class ServiceImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [250, 250]
   end
   
-  version :admin do
+  version :index do
     process :resize_to_limit => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-  
-  def default_url
-    "/uploads/service/image/placeholder/default.png"
+  def filename
+     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
 
 end
