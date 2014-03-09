181fa7c626c9e107244013cc882aca072697741a
Terry <terry@terry-syra.(none)>
2014-03-09 17:16:17 +0100

    [ADD] User xp fonctionnel

--- Syra/app/controllers/users_controller.rb
+++ Syra/app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    UsersHelper.grant_xp(@user,150);
+    UsersHelper.grant_xp(@user,150)
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
--- Syra/app/helpers/users_helper.rb
+++ Syra/app/helpers/users_helper.rb
@@ -2,8 +2,9 @@ module UsersHelper
   
   def self.grant_xp(user,xp)
     if  (user.level.XPUser+xp)/100 > 0
-       user.level.levelUser =  (user.level.XPUser+xp)/100
+       user.level.levelUser +=  (user.level.XPUser+xp)/100
     end
     user.level.XPUser = (user.level.XPUser+xp)%100
+    user.level.save
   end
 end
