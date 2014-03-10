3061ea634959f31c9ddd65932cb748421a58ee6a
MaxBSyraDev <terry@terry-syra.(none)>
2014-03-10 19:56:30 +0100

    [Modif] Profil - Experience utilisateur avec coeff reduc + progress bar fonctionnelle

--- Syra/app/helpers/users_helper.rb
+++ Syra/app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
   
   def self.grant_xp(user,xp)
-    user.level.levelUser +=  (user.level.XPUser+xp)/100
-    user.level.XPUser = (user.level.XPUser+xp)%100
+    coeff = 1-(0.05*user.level.levelUser>=1?0.05:0.05*user.level.levelUser)
+    user.level.levelUser +=  (user.level.XPUser+(xp*coeff))/100
+    user.level.XPUser = (user.level.XPUser+(xp*coeff))%100
     user.level.save
   end
 end
