5c2330352c94da3d2eea016cecd1f2aa9a4c0531
IReivax <reivax.amok@gmail.com>
2014-03-17 14:37:41 +0100

    Add an empty page for "Equipe", rename title identification/connection

--- Syra/config/routes.rb
+++ Syra/config/routes.rb
@@ -47,6 +47,8 @@ Syra::Application.routes.draw do
   get '/users/:id/follow', :to => 'users#follow', :as => 'follow_user'
 
   get '/map', :to => 'pages#map'
+  
+  get '/equipe', :to => 'pages#equipe'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
