868f5a1febedd34a2ffbe8bdfde92c38aa246fb1
MaxBSyraDev <maxime.boutrouille@gmail.com>
2014-06-03 19:18:53 +0200

    A - map qui affiche sa propre position a la création d'un service

---
+++ Syra/app/assets/javascripts/service-new.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    var addresspicker = $( "#addresspicker" ).addresspicker({
+      componentsFilter: 'country:FR'
+    });
+    var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
+      regionBias: "fr",
+      updateCallback: showCallback,
+      mapOptions: {
+        zoom: 4,
+        center: new google.maps.LatLng($("#y").text(), $("#x").text()),
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      },
+      elements: {
+        map:      "#map",
+        lat:      "#service_address_y",
+        lng:      "#service_address_x",
+        street_number: '#street_number',
+        route: '#route',
+        locality: '#service_address_ville',
+        administrative_area_level_2: '#administrative_area_level_2',
+        administrative_area_level_1: '#service_address_region',
+        country:  '#country',
+        postal_code: '#postal_code',
+        type:    '#type'
+      }
+    });
+
+    var gmarker = addresspickerMap.addresspicker( "marker");
+    gmarker.setVisible(true);
+    addresspickerMap.addresspicker( "updatePosition");
+
+    $('#reverseGeocode').change(function(){
+      $("#addresspicker_map").addresspicker("option", "reverseGeocode", ($(this).val() === 'true'));
+    });
+
+    function showCallback(geocodeResult, parsedGeocodeResult){
+      $('#callback_result').text(JSON.stringify(parsedGeocodeResult, null, 4));
+    }
+    // Update zoom field
+    var map = $("#addresspicker_map").addresspicker("map");
+    google.maps.event.addListener(map, 'idle', function(){
+      $('#zoom').val(map.getZoom());
+    });
+
+});
