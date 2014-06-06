14d0c95556c5cee7b43479ed6976611cd451eab0
Snaaky <julien.duquesnoy8@gmail.com>
2014-06-06 16:22:32 +0200

    M - price des services + money des utilisateurs

--- Syra/db/seeds.rb
+++ Syra/db/seeds.rb
@@ -25,7 +25,7 @@ a1 = Address.create(label:'185 Residence Sophora 59283 Moncheaux', y: 50.4528437
 l1 = Level.create(levelUser:1,XPUser:0)
 
 
-user1 = User.create!(name: 'Maxime', lastName: 'Prudhomme', password:'12345678', email:'prudhommemaxime@gmail.com', money: '12',
+user1 = User.create!(name: 'Maxime', lastName: 'Prudhomme', password:'12345678', email:'prudhommemaxime@gmail.com', money: '20',
            phone:'0601280156',biography:'Bla Bla Biographie',isPremium:false,level_id:l1.id,address_id:a1.id,
            email_confirmation:'prudhommemaxime@gmail.com',password_confirmation:'12345678',accept_conditions:true, isAdmin:true)
 
@@ -34,7 +34,7 @@ a2 = Address.create(label:'rue Nationale Lille 59000', y: 50.6328407, x: 3.05275
 l2 = Level.create(levelUser:1,XPUser:0)
 
 
-user2 = User.create!(name: 'Maxime', lastName: 'Boutrouille', password:'12345678', email:'boutrouillemaxime@gmail.com', money: '12',
+user2 = User.create!(name: 'Maxime', lastName: 'Boutrouille', password:'12345678', email:'boutrouillemaxime@gmail.com', money: '1337',
            phone:'0601280156',biography:'Bla Bla Biographie',isPremium:false,level_id:l2.id,address_id:a2.id,
            email_confirmation:'boutrouillemaxime@gmail.com',password_confirmation:'12345678',accept_conditions:true, isAdmin:true)
 
@@ -42,7 +42,7 @@ a3 = Address.create(label:'83 rue Jacquemars gielee', y: 50.6341846, x: 3.054211
 l3 = Level.create(levelUser:1,XPUser:0)
 
 
-user3 = User.create!(name: 'Xavier', lastName: 'Koma', password:'12345678', email:'xavierkoma@gmail.com', money: '12',
+user3 = User.create!(name: 'Xavier', lastName: 'Koma', password:'12345678', email:'xavierkoma@gmail.com', money: '20',
            phone:'0601280156',biography:'Bla Bla Biographie',isPremium:false,level_id:l3.id,address_id:a3.id,
            email_confirmation:'xavierkoma@gmail.com',password_confirmation:'12345678',accept_conditions:true, isAdmin:true)
 
@@ -57,10 +57,6 @@ user4 = User.create!(name: 'Julien', lastName: 'Duquesnoy', password:'12345678',
 a5 = Address.create(label:'25 rue de Calais', y: 50.634398, x: 3.044907500000022, region:'Nord-Pas-de-Calais', ville: 'Lille')
 l5 = Level.create(levelUser:1,XPUser:0)
 
-
-#user5 = User.create!(name: 'Terry', lastName: 'Mainvis', password:'12345678', email:'terry.mainvis@gmail.com', money: '12',
- #          phone:'0600000000',biography:'Bla Bla Biographie',isPremium:false,level_id:l5.id,address_id:a5.id,
-#           email_confirmation:'terry.mainvis@gmail.com',password_confirmation:'12345678',accept_conditions:true)
            
 Hobby.create(label:'Informatique')
 Hobby.create(label:'Natation')
@@ -71,37 +67,37 @@ Hobby.create(label:'Mécanique')
 Hobby.create(label:'Lecture')
 
 
-s1 = Service.create(title: 'cours informatique',price: 20,description: 'JE SUIS UN GEEK',disponibility: 'quand tu veux bb',
+s1 = Service.create(title: 'cours informatique',price: 4,description: 'JE SUIS UN GEEK',disponibility: 'quand tu veux bb',
                isGiven: true, isFinished: true,
                address_id: a1.id,category_id: c1.id,user_id:user2.id)
 
 
-s2 = Service.create(title: 'cours Rails',price: 30,description: 'Ruby !!',disponibility: 'a partir de 18h',
+s2 = Service.create(title: 'cours Rails',price: 5,description: 'Ruby !!',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a2.id,category_id: c1.id,user_id:user2.id)
 
 
-s3 = Service.create(title: 'Jardinage',price: 30,description: 'surtout les fleurs',disponibility: 'a partir de 18h',
+s3 = Service.create(title: 'Jardinage',price: 2,description: 'surtout les fleurs',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a3.id,category_id: c2.id,user_id:user1.id)
 
 
-Service.create(title: 'BabySitting',price: 40,description: 'serieux et disponible',disponibility: 'a partir de 18h',
+Service.create(title: 'BabySitting',price: 5,description: 'serieux et disponible',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a4.id,category_id: c3.id,user_id:user1.id)
 
 
-Service.create(title: 'cours BDD',price: 30,description: 'Love Francis',disponibility: 'a partir de 18h',
+Service.create(title: 'cours BDD',price: 3,description: 'Love Francis',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a4.id,category_id: c1.id,user_id:user1.id)
 
 
-Service.create(title: 'Coupe de cheveux',price: 30,description: 'Etudiant',disponibility: 'a partir de 18h',
+Service.create(title: 'Coupe de cheveux',price: 6,description: 'Etudiant',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a5.id,category_id: c3.id,user_id:user1.id)
 
 
-Service.create(title: 'cours de piano',price: 30,description: 'medaille dor',disponibility: 'a partir de 18h',
+Service.create(title: 'cours de piano',price: 8,description: 'medaille dor',disponibility: 'a partir de 18h',
                isGiven: true, isFinished: false,
                address_id: a1.id,category_id: c4.id,user_id:user1.id)
 
