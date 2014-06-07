ded1124310e97051f925e2d7a7633515d890b5f8
MaxBSyraDev <maxime.boutrouille@gmail.com>
2014-06-07 15:46:28 +0200

    M - visuel notifs

--- Syra/app/assets/javascripts/notifications.js
+++ Syra/app/assets/javascripts/notifications.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
 $(document).ready(function() {
 	$('.deletenotif').click(function() {
 		var li = this.parentNode.parentNode;
-		li.remove();
-		if ($(".notifmenu").children().size()==1) {
-			$("#toutsupprnotif").remove();
-			$(".notifmenu").append("<li><a href=#>Aucune notification</a></li>");
-		}
+		$(li).fadeOut(400, function() {
+			li.remove();
+			if ($(".notifmenu").children().size()==1) {
+				$("#toutsupprnotif").remove();
+				$(".notifmenu").append("<li><a href=#>Aucune notification</a></li>");
+			}
+		});
 		$('.dropdown-menu').dropdown('toggle');
 		$.ajax({
 		  url: "/notifications/" + $(this).attr('data-id'),
