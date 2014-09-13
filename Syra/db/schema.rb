8a6220397915dc8e3331a8b205dcbeb385c16a83
Olin <Prudhommemaxime@gmail.com>
2014-09-13 18:17:51 +0200

    Favorite ...

--- Syra/db/schema.rb
+++ Syra/db/schema.rb
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 20140913152435) do
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
+    t.boolean "favorite"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
